2b7a7ae535fa9f46168e5f2f74daf5189921951a
UMTPFE <47662521+umtpfe@users.noreply.github.com>
2019-03-11 17:47:00 +0100

    exemple  pour le thread

---
+++ thread.py
@@ -0,0 +1,16 @@
+import threading
+import time
+
+def sleeper (n, name):
+	print('hi, I am{}. Going to sleep for 5 seconds \\n'.format(name))
+	time.sleep(n)
+	print('{} has woken up from sleep \n'.format(name))
+
+
+t = threading.Thread(target = sleeper, name = 'thread1', args = (5, 'thread1'))
+
+t.start()
+t.join()
+
+print('hello')
+print('hello')	 
